ac3ffa6a39ffe89854bd76fdec3094e1be5dd000
Marvish <mschandr@uci.edu>
2022-08-04 12:20:27 -0700

    Determined the various conditions of an outdoor and indoor social for workers to partake in after work.

--- worksocial.java
+++ worksocial.java
@@ -9,7 +9,14 @@ public class worksocial {
 public class indoorSocial{
     public static void main(String [] args){
         personality = "smart";
-        if personality
+        if (personality = "strategic")
+            System.out.print("As a team, we should either play the following: checkers, chess, or a puzzle.");
+        if (personality = "initiator")
+            System.out.print("As a team, let's build a lego set together.");
+        if (personality = "fun")
+            System.out.print("As a team, let's watch a movie together, so we can talk about it afterwards.");
+        else:
+        System.out.print("As a team, let's share our personality type, so we can do an activity.)
     }
 }
 
@@ -17,7 +24,17 @@ public class indoorSocial{
 
 public class outdoorSocial{
     public static void main(String [] args){
-
+        personality = "hungry";
+        if (personality = "hungry")
+            System.out.print("We're starving, let's go to a nearby restraurant to eat together.");
+        if (personality = "funny")
+            System.out.print("It's been a long day at work. Let's make some laughs and watch comedy together.");
+        if (personality = "interesting")
+            System.out.print("Hey we're all in an interesting attitude today, let's do an escape room.");
+        if (personality = "energetic")
+            System.out.print("Our team should do something energetic, let's go to the nearby gym and a high interval exercise.");
+        else:
+        System.out.print("As a team, let's figure out an activity that we can do outside.")
     }
 }
 
