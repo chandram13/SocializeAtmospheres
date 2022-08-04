ebb1b3ae4d8e2810084fe08b882e498253e2f82f
Marvish <mschandr@uci.edu>
2022-08-04 11:29:53 -0700

    Had to setup function conditions

---
+++ worksocial.java
@@ -0,0 +1,24 @@
+
+
+public class worksocial {
+    public static void main(String[] args){
+        System.out.printIn("Are you either an indoor or outdoor person? Here are the scenarios you can socialize in!")
+    }
+}
+
+public class indoorSocial{
+    public static void main(String [] args){
+        personality = "smart";
+        if personality
+    }
+}
+
+
+
+public class outdoorSocial{
+    public static void main(String [] args){
+
+    }
+}
+
+
